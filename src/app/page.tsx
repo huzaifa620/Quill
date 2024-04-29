a0189a9884972b81aef54dffb8178a6b2e700bbf
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2024-04-30 00:50:11 +0500

    initialization

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import MaxWidthWrapper from "./components/maxWidthWrapper";
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper className="bg-red-500">
+      HELLO WORLD
+    </MaxWidthWrapper>
+  );
+}
